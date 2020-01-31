54abfc977dc0b522291651bcb551fdcfdf755c64
David Granström <david@davidgranstrom.com>
2020-01-31 12:46:21 +0100

    Re-order source file

--- src/utils.c
+++ src/utils.c
@@ -18,7 +18,46 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "slow/utils.h"
 
+#define CHUNK_SIZE 512
 #define NANO_SECONDS 1000000
+
+int read_stream(FILE *f, char **data, size_t *size)
+{
+  size_t readsz, totalsz = 0, memsz;
+  char *buf, *tmpbuf;
+  int ret = 0;
+  clearerr(f);
+  memsz = CHUNK_SIZE;
+  buf = malloc(memsz);
+  if (buf != NULL) {
+    while (ret == 0) {
+      readsz = fread(buf + totalsz, 1, CHUNK_SIZE, f);
+      if (ferror(f)) {
+        ret = errno;
+      } else {
+        totalsz += readsz;
+        if (totalsz >= memsz) {
+          memsz += CHUNK_SIZE;
+          tmpbuf = realloc(buf, memsz);
+          if (tmpbuf == NULL) {
+            ret = errno;
+          } else
+            buf = tmpbuf;
+        }
+      }
+      if (readsz != CHUNK_SIZE)
+        break;
+    }
+  } else
+    ret = errno;
+  if (ret) {
+    free(buf);
+    return ret;
+  }
+  *data = buf;
+  *size = totalsz;
+  return 0;
+}
 
 void slow_sleep(int millis)
 {
@@ -34,43 +73,4 @@ void slow_sleep(int millis)
 double slow_random()
 {
   return drand48();
-}
-
-int read_stream(FILE *f, char **data, size_t *size)
-{
-    #define CHUNK_SIZE 512
-    size_t readsz, totalsz = 0, memsz;
-    char *buf, *tmpbuf;
-    int ret = 0;
-    clearerr(f);
-    memsz = CHUNK_SIZE;
-    buf = malloc(memsz);
-    if (buf != NULL) {
-        while (ret == 0) {
-            readsz = fread(buf + totalsz, 1, CHUNK_SIZE, f);
-            if (ferror(f)) {
-                ret = errno;
-            } else {
-                totalsz += readsz;
-                if (totalsz >= memsz) {
-                    memsz += CHUNK_SIZE;
-                    tmpbuf = realloc(buf, memsz);
-                    if (tmpbuf == NULL) {
-                        ret = errno;
-                    } else
-                        buf = tmpbuf;
-                }
-            }
-            if (readsz != CHUNK_SIZE)
-                break;
-        }
-    } else
-        ret = errno;
-    if (ret) {
-        free(buf);
-        return ret;
-    }
-    *data = buf;
-    *size = totalsz;
-    return 0;
 }
